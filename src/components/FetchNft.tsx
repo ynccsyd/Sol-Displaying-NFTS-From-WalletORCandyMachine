452a42d6a2549cd38727e5c8df69f026953cef56
ynccsyd <muhibbi_10_@hotmaile.com>
2022-12-17 12:13:13 +0300

    Added All

--- src/components/FetchNft.tsx
+++ src/components/FetchNft.tsx
@@ -67,25 +67,3 @@ export const FetchNft: FC = () => {
   )
 }
 
-{/* <div className="nft-card ">
-      <div className="inner-wrapper">
-        <div className="content">
-          <img
-            src={nft?.meta?.content[0]?.url}
-            alt={nft?.meta?.name}
-            className="imgnft"
-          />
-          <div className="nft-info">
-            <div className="nft-info-l">
-              <div className="nft-info-name">{nft?.meta?.name}</div>
-              <p className="price-tag">
-                {nft?.lastSale?.price}
-                ETH
-              
-              </p>
-            </div>
-        
-          </div>
-        </div>
-      </div>
-    </div> */}
